7d88bc9a4638c5c06f740387885c0d775e362cd0
Joshua Cheung <contact.cjoshua@gmail.com>
2022-06-27 20:11:03 -0700

    complete exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -1,10 +1,20 @@
-require_relative '../setup'
-require_relative './exercise_1'
-require_relative './exercise_2'
-require_relative './exercise_3'
-require_relative './exercise_4'
+require_relative "../setup"
+require_relative "./exercise_1"
+require_relative "./exercise_2"
+require_relative "./exercise_3"
+require_relative "./exercise_4"
 
 puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@total_revenue = Store.sum("annual_revenue")
+@all_store = Store.count
+
+# output the total annual revenue
+p @total_revenue
+# output the average annual revenue of all store
+p @total_revenue / @all_store
+# output number of store total ann rev more than 1m
+p Store.where("annual_revenue > ?", 1_000_000).count
